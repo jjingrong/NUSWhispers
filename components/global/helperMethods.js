6b2dee71645fd150deaea6de59a63147c67f04db
JingRong ( <jing_rong92@hotmail.com>
2016-03-12 17:16:10 -0800

    added comments for global methods file

--- components/global/helperMethods.js
+++ components/global/helperMethods.js
@@ -1,6 +1,16 @@
 'use strict';
-var Global = {
-  // Change javascript array into object
+
+/**
+  * This file is for including helper methods when needed
+  * Especially for frequently used javascript methods that will be used across files
+*/
+
+
+var GlobalMethods = {
+  /** Change javascript array into object
+    * @params: array
+    * @return: object
+  */
   arrToObject(arr) {
     var rv = {};
     for (var i = 0; i < arr.length; ++i)
@@ -9,5 +19,4 @@ var Global = {
   }
 }
 
-
-module.exports = Global;
+module.exports = GlobalMethods;
